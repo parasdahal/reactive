6c4a83c928d6ea77a2f953ec6d36738d15e10af8
parasdahal <shree5paras@gmail.com>
2015-10-03 22:55:32 +0530

    Added Klogger to Composer dependencies

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Slim' => array($vendorDir . '/slim/slim'),
+    'Psr\\Log\\' => array($vendorDir . '/psr/log'),
 );
